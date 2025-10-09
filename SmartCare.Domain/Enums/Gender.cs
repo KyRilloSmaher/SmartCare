76b4c2dbf3e9f2aacce2680c90752d30b56f8759
kyrillos7 <156196824+KyRilloSmaher@users.noreply.github.com>
2025-10-09 19:52:01 +0300

    Merge Enums directory from Enums branch

---
+++ SmartCare.Domain/Enums/Gender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCare.Domain.Enums
+{
+    public enum Gender
+    {
+        NotPreferToSay = 0,
+        Male = 1,
+        Female = 2
+    }
+}
