2ff4b27705ec497b62620b7e7e35194377187fdc
kyrillos7 <156196824+KyRilloSmaher@users.noreply.github.com>
2025-10-11 14:52:28 +0300

    Adding Application Db Context

--- SmartCare.Domain/Entities/Client.cs
+++ SmartCare.Domain/Entities/Client.cs
@@ -1,11 +1,7 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿
+using Microsoft.AspNetCore.Identity;
 using SmartCare.Domain.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Principal;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace SmartCare.Domain.Entities
 {
