ada0a46e349bad13b57575ca45302c834f1f31b2
kyrillos7 <156196824+KyRilloSmaher@users.noreply.github.com>
2025-11-03 08:14:02 +0200

    Create Basic Inventory Interface

---
+++ SmartCare.Domain/IRepositories/IInventoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using SmartCare.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCare.Domain.IRepositories
+{
+    public interface IInventoryRepository : IGenericRepository<Inventory>
+    {
+        Task<Guid> GetBestInventoryIdAsync(Guid productId ,int quantityRequired);
+        Task<Inventory> GetAvalaibleStockForProductAsync(Guid productId);
+        Task<int> GetTotalStockForProductAsync(Guid productId);
+        Task<Inventory> IncreaseProductStockAsync(Guid InventoryId, int quantityToAdd);
+        Task<Inventory> DecreaseProductStockAsync(Guid InventoryId, int quantityToSubtract);
+        Task<Inventory> GetStockOfProductInStore(Guid productId, Guid storeId);
+        Task<List<Inventory>> GetAllInventoryInStoreAsync(Guid storeId);
+
+
+    }
+}
