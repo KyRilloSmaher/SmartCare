c18ee548812765883516014591cc664224f99cf3
kyrillos7 <156196824+KyRilloSmaher@users.noreply.github.com>
2025-11-03 08:18:12 +0200

    Create Basic Order Repo

--- SmartCare.Domain/IRepositories/IOrderRepository.cs
+++ SmartCare.Domain/IRepositories/IOrderRepository.cs
@@ -1,4 +1,5 @@
 ﻿using SmartCare.Domain.Entities;
+using SmartCare.Domain.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,10 @@ namespace SmartCare.Domain.IRepositories
 {
     public interface IOrderRepository : IGenericRepository<Order>
     {
+        Task<IEnumerable<Order>> GetOrdersByCustomerIdAsync(Guid customerId);
+        Task<IEnumerable<Order>> GetOrdersWithDetailsAsync();
+        Task<Order> GetOrderWithDetailsByIdAsync(Guid orderId);
+        Task<IEnumerable<Order>> GetOrdersByStatus(OrderStatus status);
+
     }
 }
