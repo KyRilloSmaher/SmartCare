5f0fc9243194de2ced7b4d676d2ea33870d355ab
kyrillos7 <156196824+KyRilloSmaher@users.noreply.github.com>
2026-03-14 15:38:40 +0200

    Fix Add to cart Bug

--- SmartCare.Application/Features/Cart/Commands/AddToCart/AddToCartCommandHandler.cs
+++ SmartCare.Application/Features/Cart/Commands/AddToCart/AddToCartCommandHandler.cs
@@ -74,8 +74,8 @@ namespace SmartCare.Application.Features.Carts.Commands.AddToCart
             cartItem.UnitPrice = product.Price;
             cartItem.SubTotal = product.Price * cartItem.Quantity;
             cartItem.InventoryId = inventory.Id;
-            await cart.Items.Add(cartItem);
-            await cart.ReCalculateTotalPrice();
+            await _unitOfWork.Carts.AddCartItemAsync(cartItem);
+            cart.ReCalculateTotalPrice();
             await _unitOfWork.SaveChangesAsync();
             var responseDto = _mapper.Map<CartItemResponseDto?>(cartItem);
             _backgroundJobService.Enqueue(() => CleanUpCart(cartItem.CartItemId, cart.ClientId));
