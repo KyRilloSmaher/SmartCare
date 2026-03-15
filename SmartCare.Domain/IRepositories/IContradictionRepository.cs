d390e6719fff9cb23ee7df56a833bea13258e487
kyrillos7 <156196824+KyRilloSmaher@users.noreply.github.com>
2026-03-15 18:06:18 +0200

    Add Contradiction Repository

---
+++ SmartCare.Domain/IRepositories/IContradictionRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartCare.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCare.Domain.IRepositories
+{
+    public interface IContradictionRepository : IGenericRepository<Contradiction>
+    {
+       Task<Contradiction?> ContradictionExistsAsync(string ingredientA, string ingredientB);
+    }
+}
---
+++ SmartCare.InfraStructure/Repositories/ContradictionRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartCare.Domain.Entities;
+using SmartCare.Domain.IRepositories;
+using SmartCare.InfraStructure.DbContexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCare.InfraStructure.Repositories
+{
+    public class ContradictionRepository : GenericRepository<Contradiction>,IContradictionRepository
+    {
+        public ContradictionRepository(ApplicationDBContext context):base(context) { }
+
+        public async Task<Contradiction?> ContradictionExistsAsync(string ingredientA, string ingredientB)
+        {
+            return await _context.Contradictions
+                .FirstOrDefaultAsync(c =>
+                    (c.Ingredient_A == ingredientA && c.Ingredient_B == ingredientB) ||
+                    (c.Ingredient_A == ingredientB && c.Ingredient_B == ingredientA)
+                );
+        }
+    }
+}
